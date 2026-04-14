11f04eaf5ec3da5e45e96ec44184210c421c44e5
kawae0709 <kawae0709@gmail.com>
2026-04-15 01:22:28 +0800

    1

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-rc app.ico
-cl /O2 /EHsc /MT /std:c++17 /DUNICODE /D_UNICODE /Fe:ZenCrop.exe main.cpp Utils.cpp OverlayWindow.cpp ReparentWindow.cpp ThumbnailWindow.cpp app.res user32.lib gdi32.lib dwmapi.lib shcore.lib shell32.lib
+if not exist build mkdir build
+rc /fo build\app.res app.ico
+cl /O2 /EHsc /MT /std:c++17 /DUNICODE /D_UNICODE /Fobuild\ /Fe:build\ZenCrop.exe main.cpp Utils.cpp OverlayWindow.cpp ReparentWindow.cpp ThumbnailWindow.cpp build\app.res user32.lib gdi32.lib dwmapi.lib shcore.lib shell32.lib
 if %ERRORLEVEL% equ 0 echo Build Success!
